7f716480f8ba3a1e9a950f1c08ca5ceff4706f1d
Kien Quoc <kienquocc27@gmail.com>
2024-11-15 10:16:55 +0000

    placeholder and visual

--- Assets/Script/PlayerCardList.cs
+++ Assets/Script/PlayerCardList.cs
@@ -1,13 +1,22 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCardList : CardList
 {
-    [SerializeField] private GameObject cardPrefab;
+    [SerializeField] private GameObject cardPlaceholderPrefab;
+    [SerializeField] private GameObject cardVisualPrefab;
+    [SerializeField] private Transform _cardPlaceholdersContainerTransform;
+    [SerializeField] private Transform _cardViualsContainerTransform;
 
     protected override void UpdateView()
     {
-        foreach (Transform child in transform)
+        foreach (Transform child in _cardPlaceholdersContainerTransform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (Transform child in _cardViualsContainerTransform)
         {
             Destroy(child.gameObject);
         }
@@ -20,19 +29,24 @@ public class PlayerCardList : CardList
 
     public void ShowCardGameObject(CardData cardData, int index, Vector3 position)
     {
-        GameObject cardInstance = Instantiate(cardPrefab, transform); 
-        cardInstance.transform.SetParent(transform);
-        cardInstance.transform.localPosition = position;
-        cardInstance.transform.localRotation = Quaternion.identity;
-        cardInstance.transform.localScale = Vector3.one;
+        GameObject cardPlaceholderInstance = Instantiate(cardPlaceholderPrefab, _cardPlaceholdersContainerTransform);
+        cardPlaceholderInstance.transform.SetParent(_cardPlaceholdersContainerTransform);
+        cardPlaceholderInstance.transform.localPosition = position;
+        cardPlaceholderInstance.transform.localRotation = Quaternion.identity;
+        cardPlaceholderInstance.transform.localScale = Vector3.one;
 
-        CardPresenter cardPresenter = cardInstance.GetComponent<CardPresenter>();
-        cardPresenter.SetCardData(cardData);
-        cardPresenter.SetPlayerCardList(this);
-        cardPresenter.SetIndex(index);
-        if (gameObject)
-        {
-            cardPresenter.ShowImage();
-        }
+        CardPresenter cardPlaceHolderPresenter = cardPlaceholderInstance.GetComponent<CardPresenter>();
+        cardPlaceHolderPresenter.SetCardData(cardData);
+        cardPlaceHolderPresenter.SetPlayerCardList(this);
+        cardPlaceHolderPresenter.SetIndex(index);
+
+        GameObject cardVisualInstance = Instantiate(cardVisualPrefab, _cardViualsContainerTransform);
+        cardVisualInstance.transform.SetParent(_cardViualsContainerTransform);
+        cardVisualInstance.transform.localPosition = position;
+        cardVisualInstance.transform.localRotation = Quaternion.identity;
+        cardVisualInstance.transform.localScale = Vector3.one;
+
+        cardVisualInstance.GetComponent<Image>().sprite = cardData.Image;
+        
     }
 }
